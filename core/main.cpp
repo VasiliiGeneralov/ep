263acee991a30ff30e8e2f254d28627a69ab6985
Vasilii Generalov <vasilii.generalov@yandex.ru>
2023-11-21 19:34:07 +0300

    Setup qt6
    Qt6 dependency is setup in this commit.
    
    Signed-off-by: Vasilii Generalov <vasilii.generalov@yandex.ru>

--- core/main.cpp
+++ core/main.cpp
@@ -1,1 +1,11 @@
-int main() {}
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char **argv) {
+  QGuiApplication app(argc, argv);
+  QQmlApplicationEngine engine;
+
+  engine.load(QUrl("gui/main.qml"));
+
+  return app.exec();
+}
---
+++ gui/main.qml
@@ -0,0 +1,7 @@
+import QtQuick.Controls
+
+ApplicationWindow {
+  width: 1024
+  height: 768
+  visible: true
+}
